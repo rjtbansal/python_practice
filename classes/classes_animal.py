43a3304655870f146bb576b5a869e2e2bd09b26c
rjtbansal <rjtbansal@yahoo.com>
2017-03-26 15:28:22 -0700

    Added setter

--- classes/classes_animal.py
+++ classes/classes_animal.py
@@ -7,6 +7,10 @@ class Animal():
         return self.color
     def makeNoise(self):
         return self.sound
+    def setColor(self,color):
+        self.color=color
+    def setNoise(self,sound):
+        self.sound=sound
 
 #Dog class inherits from Animal..in python we just pass the class name as an arg
 class Dog(Animal):    
@@ -17,3 +21,8 @@ print(myPet.getColor())
 print(myPet.makeNoise())
 myPet.color='white'
 print(myPet.getColor())
+
+myPet.setColor('blue')
+myPet.setNoise('barks')
+print(myPet.getColor())
+print(myPet.makeNoise())
